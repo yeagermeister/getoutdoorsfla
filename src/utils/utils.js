41b2f15249946a929e08ccaa01fed46163eb2db5
yeagermeister <johnyeager75@gmail.com>
2023-04-05 12:51:03 -0400

    building helpers

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,11 +4,17 @@ let longitude;
 
 export function getLocation() {
     if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-          latitude = position.coords.latitude;
-          longitude = position.coords.longitude;
-          console.log("Latitude: " + latitude + ", Longitude: " + longitude);
-        });
+            navigator.geolocation.getCurrentPosition(function(position) {
+                latitude = position.coords.latitude;
+                longitude = position.coords.longitude;
+                console.log("Latitude: " + latitude + ", Longitude: " + longitude);
+            },
+            function(error) {
+                if (error.code === error.PERMISSION_DENIED) {
+                    alert('Please turn on location services for this site to function.  We are not sharing your coordinates with Big Brother.')
+                }
+            }
+        );
     } else {
         console.log("Geolocation is not supported by this browser.");
     }
