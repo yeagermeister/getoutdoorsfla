c86720f417c6dca2cac4e05b29500b59d89585aa
yeagermeister <johnyeager75@gmail.com>
2023-04-08 11:18:30 -0400

    updates

--- client/src/components/navbar/Start.js
+++ client/src/components/navbar/Start.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import Navbar from './Navbar';
 import SearchBar from './SearchBar';
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
