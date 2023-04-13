31db0116deae89e6eea3db6cf5df0e9e3a20c7ed
yeagermeister <johnyeager75@gmail.com>
2023-04-13 13:47:42 -0400

    jwt and auth framework in place on the frontend.  Creating a user is not hitting the mongo db though.

--- client/src/components/pages/Signup.js
+++ client/src/components/pages/Signup.js
@@ -1,23 +1,73 @@
-import '../../form.css';
-import saveData from '../../utils/saveData';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../../utils/mutations';
+
+import Auth from '../../utils/auth';
 
 const Signup = () => {
+  const [formState, setFormState] = useState({
+    username: '',
+    email: '',
+    password: '',
+  });
+  const [addUser, { error, data }] = useMutation(ADD_USER);
 
-    function handleSubmit(data){
+  const handleChange = (event) => {
+    const { name, value } = event.target;
 
-    };
-    return (
-        <>
-            <h2>Sign Up!</h2>
-            <form onSubmit={handleSubmit(data => saveData(data))}>
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+
+    try {
+      const { data } = await addUser({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <>
+        <h2>Sign Up!</h2>
+          <div className="card-body">
+            {data ? (
+              <p>
+                Success! You may now head{' '}
+                <Link to="/">back to the homepage.</Link>
+              </p>
+            ) : (
+              <form onSubmit={handleFormSubmit}>
                 <label>Username:</label>
-                <input className="spacing" name="username"/>
+                <input className="spacing"  name="username" type="text" value={formState.name} onChange={handleChange} />
+                <label>Email Address:</label>
+                <input
+                  className="spacing" name="email" type="email" value={formState.email} onChange={handleChange}  />
                 <label>Password:</label>
-                <input className="spacing" name="password" />
-                <input type="submit" className="myButton" />
-            </form>
-        </>
-    );
+                <input className="spacing" name="password" type="password" value={formState.password} onChange={handleChange} />
+                <button  className="myButton" type="submit">Submit</button>
+              </form>
+            )}
+
+            {error && (
+              <div className="my-3 p-3 bg-danger text-white">
+                {error.message}
+              </div>
+            )}
+          </div>
+    </>
+  );
 };
 
 export default Signup;
