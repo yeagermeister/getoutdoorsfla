9d3f7650777cf5c05ca60170126a84551cdf92e6
yeagermeister <johnyeager75@gmail.com>
2023-04-13 12:35:07 -0400

    jwt working on the backend

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://getoudoorsfla_admin:RV7TmUKAsqzlIccV@cluster0.v9ej8dw.mongodb.net/getoutddorsfla');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://getoudoorsfla_admin:RV7TmUKAsqzlIccV@cluster0.v9ej8dw.mongodb.net/getoutdoorsfla');
 
 module.exports = mongoose.connection;
--- serverbackup/config/connection.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://getoudoorsfla_admin:RV7TmUKAsqzlIccV@cluster0.v9ej8dw.mongodb.net/testretryWrites=true&w=majority');
-
-module.exports = mongoose.connection;
