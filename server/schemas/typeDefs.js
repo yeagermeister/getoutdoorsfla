80bfb4f933e768cc4da83482a9caf4fa7a9123ad
yeagermeister <johnyeager75@gmail.com>
2023-04-16 16:50:59 -0400

    newsite working via graphql - derver side cleaned up

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -54,7 +54,7 @@ const typeDefs = gql`
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
-    addSite(siteName: String!, description: String!, zipcode: Int!) : NewSite
+    addSite(siteName: String!, description: String!, zipcode: Int!, camping: Boolean, pets: Boolean, statepark: Boolean, park: Boolean, beach: Boolean, swimmingHole: Boolean, spring: Boolean, free: Boolean): NewSite
     login(email: String!, password: String!): Auth
     addProdSite(siteName: String!, description: String!, imageURL: String!, zipcode: Int!, camping: Boolean, pets: Boolean, statepark: Boolean, park: Boolean, beach: Boolean, swimmingHole: Boolean, spring: Boolean, free: Boolean, lat: Int!, lon: Int!): Site
   }
