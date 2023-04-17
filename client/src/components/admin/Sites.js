7a683dc384e7892621f2f4f5fb075c80063fc30b
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2023-04-16 23:42:52 -0400

    oof

--- client/src/components/admin/Sites.js
+++ client/src/components/admin/Sites.js
@@ -1,48 +1,52 @@
-import React from 'react';
-import { useQuery, gql } from '@apollo/client';
+import React from "react";
+import { useQuery, gql } from "@apollo/client";
 
 const Sites = () => {
-    const SITE_QUERY= gql`
+  const SITE_QUERY = gql`
     query getSite {
-        sites {
-            _id
-            siteName
-            description
-            imageUrl
-            distance
-            weather
-            zipcode
-            camping
-            pets
-            statepark
-            park
-            beach
-            swimmingHole
-            spring
-            free
-            lat
-            long
-          }
+      sites {
+        _id
+        siteName
+        description
+        imageUrl
+        distance
+        weather
+        zipcode
+        camping
+        pets
+        statepark
+        park
+        beach
+        swimmingHole
+        spring
+        free
+        lat
+        long
+      }
     }
-    `;
+  `;
 
-    const { data, loading, error } = useQuery(SITE_QUERY);
+  const { data, loading, error } = useQuery(SITE_QUERY);
 
-    if (loading) return "Loading...";
-    if (error) return <pre>{error.message}</pre>
-
-    return (
-        <>
-        <div>
-            <h2>Sites</h2>
-            <ul>
-                {data.sites.map((site) => (
-                    <li key={site._id}>{site._id}, {site.siteName}, <button id={site._id}>Edit Site</button></li>
-                ))};
-            </ul>
-        </div>
-        </>
-    );
-;}
+  if (loading) return "Loading...";
+  if (error) return <pre>{error.message}</pre>;
+const toggleSite = 0
+  return (
+    <>
+      <div>
+        <h2>Sites</h2>
+        <ul>
+          {data.sites.map((site) => (
+            <li key={site._id}>
+              {site._id}, {site.siteName},{" "}
+              <button id={site._id} onClick={toggleSite}>Edit Site</button>
+            </li>
+          ))}
+          ;
+        </ul>
+      </div>
+    </>
+  );
+};
 
 export default Sites;
