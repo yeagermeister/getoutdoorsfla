4784de2d351a0eed79018e0dbe0dc6e751ce162b
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2023-04-20 19:54:30 -0400

    backend front end db interaction done

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -11,6 +11,17 @@ query users {
 }
 `;
 
+export const FIND_ONE_USER=gql`
+query Query($username: String!) {
+  findOneUser(username: $username) {
+    username
+    email
+    admin
+    _id
+  }
+}
+`;
+
 export const NEWSITE_QUERY= gql`
 query sites {
   findAllNewSites {
@@ -29,6 +40,43 @@ query sites {
   }
 }
 `;
+
+export const FIND_ONE_SITE = gql`
+query Query($siteName: String!) {
+  findOneSite(siteName: $siteName) {
+    _id
+    beach
+    camping
+    comment {
+      comment
+      commentId
+      createdAt
+      username {
+        username
+        _id
+      }
+    }
+    zipcode
+    swimmingHole
+    statepark
+    spring
+    siteName
+    pets
+    park
+    lon
+    lat
+    imageURL
+    free
+    description
+    rating {
+      rating
+      ratingId
+        }
+      }
+    }
+  }
+}
+`
 
 export const SITE_QUERY = gql`
 query sites {
@@ -50,6 +98,20 @@ export const FIND_ONE_NEWSITE = gql`
 query FindOneNewSite($findOneNewSiteId: ID!) {
   findOneNewSite(_id: $findOneNewSiteId) {
     _id
+  }
+}
+`;
+
+export const FIND_USER_COMMENTS = gql`
+query Query($username: String!) {
+  findUserComments(username: $username) {
+    comment
+    commentId
+    createdAt
+    site {
+      _id
+      siteName
+    }
   }
 }
 `
