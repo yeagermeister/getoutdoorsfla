595e21f5540414cbc955621147e0ff35510a7a7b
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2023-04-22 12:38:09 -0400

    added logo and title back

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -37,7 +37,13 @@ const Navbar = (props) => {
   return (
     <nav className="navbar navbar-expand-lg">
       <div className="container-fluid">
-        {/* ... */}
+      <Link className="navbar-brand" to="/">
+          <img src='/images/logo.png' className="icon" alt="get outdoors florida" />
+        </Link>
+        <Link className='nav-link nav-item' to='/'>Get Outdoors Florida</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             {Auth.loggedIn() ? (
