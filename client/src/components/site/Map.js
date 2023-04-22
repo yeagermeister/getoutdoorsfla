b2b82fd0a792d7996cb78329f4567132f96381bd
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2023-04-21 20:26:37 -0400

    added map

---
+++ client/src/components/site/Map.js
@@ -0,0 +1,32 @@
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+require('dotenv').config();
+
+function MyMapComponent({ center, zoom }) {
+    const ref = useRef();
+
+    useEffect(() => {
+        new window.google.maps.Map(ref.current, {
+            center,
+            zoom,
+        });
+    });
+
+    return <div ref={ref} id="map" />;
+}
+
+function mapContainer(status) {
+    switch (status) {
+        case Status.LOADING:
+            return <Spinner />;
+        case Status.FAILURE:
+            return <ErrorComponent />;
+        case Status.SUCCESS:
+            return (
+                <Wrapper apiKey={process.env.GOOGLE_MAPS_API}>
+                    <MyMapComponent />
+                </Wrapper>
+            );
+    }
+}
+
+export default mapContainer;
