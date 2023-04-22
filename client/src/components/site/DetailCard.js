ece285d981e59e6d1c05fe65e262feb8f9e1354f
yeagermeister <johnyeager75@gmail.com>
2023-04-21 22:57:30 -0400

    site details updated

--- client/src/components/site/DetailCard.js
+++ client/src/components/site/DetailCard.js
@@ -3,6 +3,9 @@ import Map from '../../components/site/Map';
 import Weather from '../../components/site/Weather';
 import Comments from '../../components/site/Comments';
 const Detail = (props) => {
+    // if(props.site.free) {
+    //     return
+    // }
     return (
         <>
         <div className='col-sm'>
@@ -10,7 +13,20 @@ const Detail = (props) => {
                 <div className='col-sm-3 details'>
                     <p>{props.site.description}</p>    
                     <img src={props.site.imageURL} alt="Outdoor Imagery" />
-                    <p></p>
+                    {props.site.free && 
+                        <p>This site has free admission!</p>
+                    }
+                    <p>Tags: 
+                        {props.site.beach && <span className='tag'>Beach</span>}
+                        {props.site.pets && <span className='tag'>Pet Friendly</span>}
+                        {props.site.park && <span className='tag'>Community Park</span>}
+                        {props.site.statepark && <span className='tag'>State Park</span>}
+                        {props.site.spring && <span className='tag'>Spring</span>}
+                        {props.site.swimmingHole && <span className='tag'>Swimming Hole</span>}
+                        {props.site.camping && <span className='tag'>camping</span>}
+                    </p>
+                    <p>Average Rating:</p>
+                    <p>Your Rating:</p>
                 </div>
                 <div className='col-sm'>
                     <div className='row'>
