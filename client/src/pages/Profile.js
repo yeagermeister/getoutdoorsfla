b2fdbd2d31c1e6b8bd62ea7ef6287d4401e25e8f
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2023-04-23 16:43:40 -0400

    findusercomments working

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,27 @@
+// import React from 'react';
+
+
+// const Admin = () => {
+
+
+
+
+
+
+//     return (
+//         <>
+//         <h2>Profile</h2>
+//         <p>Welcome to your profile!</p>
+//         <aside>Rated pages:
+//             <p sites={props.site}>
+//             {sites.map(sites, )}
+
+//             </p>
+//         </aside>
+        
+        
+//         </>
+//     )
+// }
+
+// export default Admin;
