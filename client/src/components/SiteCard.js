2f88c33a90b724cf37852feea9f173532b451ba1
Benjamin Sprenger <115416353+azrazel95@users.noreply.github.com>
2023-04-23 22:38:42 -0400

    distance display works

--- client/src/components/SiteCard.js
+++ client/src/components/SiteCard.js
@@ -1,46 +1,57 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { getWeather, iconUrl } from '../utils/weather';
-import { getDistance } from '../utils/distance'
 import { latitude, longitude } from '../utils/location';
 
 const SiteCard = ({ sites }) => {
   const [enrichedSites, setEnrichedSites] = useState([]);
+  const [distances, setDistances] = useState([]);
+
   const icon = "https://" + iconUrl;
 
   useEffect(() => {
-    const getData = async () => {
+    const getDistance = (lat1, lon1, lat2, lon2) => {
+      const R = 6371e3; // metres
+      const φ1 = lat1 * Math.PI/180; // φ, λ in radians
+      const φ2 = lat2 * Math.PI/180;
+      const Δφ = (lat2-lat1) * Math.PI/180;
+      const Δλ = (lon2-lon1) * Math.PI/180;
+
+      const a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+                Math.cos(φ1) * Math.cos(φ2) *
+                Math.sin(Δλ/2) * Math.sin(Δλ/2);
+      const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+      const distance = c * R; // in metres
+      return distance / 1609.344; // convert to miles
+    };
+
+    const fetchData = async () => {
       try {
         const enrichedData = await Promise.all(sites.map(async (site) => {
-          const response= await getWeather(site.zipcode);
-          const enrichedSite = response;
-          console.log(response)
-          return {...site, ...enrichedSite};
+          const weather = await getWeather(site.zipcode);
+          return { ...site, ...weather };
         }));
         setEnrichedSites(enrichedData);
+
+        const distanceData = sites.map((site) => {
+          const distance = getDistance(site.lat, site.lon, latitude, longitude);
+          return { id: site.id, distance };
+        });
+        setDistances(distanceData);
       } catch (error) {
         console.error('Error enriching data:', error)
-    }
-    try {
-      const enrichedData = await Promise.all(sites.map(async (site) => {
-        const response= await getDistance(site.lat, site.lon, latitude, longitude);
-        const enrichedSite = response;
-        console.log(response)
-        return {...site, ...enrichedSite};
-      }));
-      setEnrichedSites(enrichedData);
-    } catch (error) {
-      console.error('Error enriching data:', error)
-  }
-  };
-  
-  getData();
-}, [sites]);
+      }
+    };
+
+    fetchData();
+  }, [sites]);
+
 
   return (
     <div className='container'>
       <div className='row w-60 m-5 p-5'>
-        {enrichedSites.map(site => (
+        {enrichedSites.map((site, index) => (
           <div key={site.id} className='col-sm'>
             <div className='card border box border-rounded shadow myCard'>
               <h5 className='card-title text-center'>{site.siteName}</h5>
@@ -55,12 +66,11 @@ const SiteCard = ({ sites }) => {
                 <p className='card-description'>{site.description}</p>
               </div>
               <p className='text-center'>
-                <span id={`distance${site.id}`} className='mr-5'>
-               miles away
+              <span id={`distance${site.id}`} className='mr-5'>
+                  {distances.find(distanceData => distanceData.id === site.id)?.distance.toFixed(0)} miles away
                 </span>
                 <Link to={`/Site/${site._id}`}><button className='btn btn-info btn-sm active myButton'>More Information</button></Link>
                 <span id={`weather${site._id}`} className='ml-5'><img src={icon} alt='weather icon' /></span>
-                
               </p>
             </div>
           </div>
@@ -69,7 +79,5 @@ const SiteCard = ({ sites }) => {
     </div>
   );
 };
-
-
 
 export default SiteCard;
