4823edb8cf98f97c584c854ed52b151cbaa1bba5
yeagermeister <johnyeager75@gmail.com>
2023-04-26 22:59:19 -0400

    updated URI to http://localhost:49017/graphql

--- client/src/App.js
+++ client/src/App.js
@@ -25,7 +25,7 @@ import { setContext } from '@apollo/client/link/context';
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3001/graphql',
+  uri: 'http://localhost:49017/graphql',
 });
 
 // Construct request middleware that will attach the JWT token to every request as an `authorization` header
